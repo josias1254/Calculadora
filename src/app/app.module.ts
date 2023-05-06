3f213f1460087ca1dffec986dfe542901035c897
josias1254 <josiasalmeida1407@gmail.com>
2023-05-06 14:03:31 -0300

    Versão Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TitleComponent } from './title/title.component';
 import { CheckSampleComponent } from './check-sample/check-sample.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent,
     CheckSampleComponent,
   ],
   imports: [
