ba54419d08fd032968dace693767067f1c51c201
josias1254 <josiasalmeida1407@gmail.com>
2023-05-06 22:28:39 -0300

    Merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { CheckSampleComponent } from './check-sample/check-sample.component';
-=======
 import { CalculadoraComponent } from './Calculadora/Calculadora.component';
->>>>>>> gh-pages
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    CheckSampleComponent,
-=======
     CalculadoraComponent,
->>>>>>> gh-pages
   ],
   imports: [
     BrowserModule
